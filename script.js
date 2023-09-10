56867aa253c5ece8954ec898073018f2e7d50402
Daniel  Matos <84199260+danielzmatos@users.noreply.github.com>
2023-09-10 15:45:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function findLyrics(artist,song){
-    return fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
+    return fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`);
 }
 
 const form = document.querySelector('#lyrics_form');
@@ -11,13 +11,13 @@ form.addEventListener('submit', el =>{
 async function doSubmit(){
     const lyrics_el = document.querySelector("#lyrics");
     const artist = document.querySelector("#artist");
-    const song = document.querySelector("#song");
+    const song = document.querySelector("#title");
 
     lyrics_el.innerHTML = '<div class="spinner-grow" role="status"><span class="sr-only">Carregando...</span></div>';
 
     //async await
     try{
-        const lyricsResponse = await findLyrics(artist.value, song.value);
+        const lyricsResponse = await findLyrics(artist.value, title.value);
         const data = await lyricsResponse.json();
         if (data.lyrics)
             lyrics_el.innerHTML = data.lyrics;
